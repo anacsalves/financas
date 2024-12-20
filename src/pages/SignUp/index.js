d2d0d3a83e71f8ee55feb5c84f68e34fa1eff78e
Ana Clara <anacsalves@unifei.edu.br>
2024-12-20 20:35:51 -0300

    criação da pagina signin

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SignuP(){
+    return(
+      <View style={styles.container}>
+        <Text>tela CADASTRO</Text>
+      </View>
+    )
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
