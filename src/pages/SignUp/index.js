c356414a38ee5f9674b33ffbeef4fa8020693f91
Ana Clara <anacsalves@unifei.edu.br>
2024-12-21 18:48:16 -0300

    criação do contexto e da pagina signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useContext} from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import {Background, Container, AreaInput, Input, SubmitButton, SubmitText} from '../SignIn/styles'
+import { AuthContext } from '../../contexts/auth';
+
 
 export default function SignuP(){
+
+  const { user } = useContext(AuthContext)
+
+  function handleSignUp(){
+    console.log(user);
+  }
+
+
     return(
-      <View style={styles.container}>
-        <Text>tela CADASTRO</Text>
-      </View>
+     <Background>
+      <Container
+        behavior={Platform.OS === 'ios' ? 'padding' : ' '}
+        enabled>
+
+          <AreaInput>
+            <Input placeholder='Nome'/>
+          </AreaInput>
+
+          <AreaInput>
+            <Input placeholder='E-mail'/>
+          </AreaInput>
+
+          <AreaInput>
+            <Input placeholder='Senha'/>
+          </AreaInput>
+
+          <SubmitButton onPress={handleSignUp}>
+            <SubmitText>Cadastrar</SubmitText>
+          </SubmitButton>
+      </Container>
+     </Background>
     )
   
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -19,6 +19,16 @@ function AuthRoutes(){
             <AuthStack.Screen
             name="SignUp"
             component={SignuP}
+            options={{
+                headerStyle:{
+                    backgroundColor: '#3b3dbf',
+                    borderBottomWidth: 1,
+                    borderBottomColor: '#00b94a'
+                },
+                headerTintColor: '#fff',
+                headerTitle: 'Voltar',
+                headerBackTitleVisible: false,
+            }}
             />
 
         </AuthStack.Navigator>
